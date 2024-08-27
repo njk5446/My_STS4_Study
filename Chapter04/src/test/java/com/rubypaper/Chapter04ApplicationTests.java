3c08d9ec40327d641bb391cc7f7149d1fcf9698c
njk5446 <njk5446@naver.com>
2024-08-27 22:34:41 +0900

    2024-08-27-22:34

--- Chapter04/src/test/java/com/rubypaper/Chapter04ApplicationTests.java
+++ Chapter04/src/test/java/com/rubypaper/Chapter04ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.rubypaper;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Chapter04ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.rubypaper;
+//
+//
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Chapter04ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
